72c3fc79f8ca3dff9ebd584e9b286da78f2c98ad
Desplat <matt-desplat@wanadoo.fr>
2019-02-20 14:37:05 +0100

    Ajout de trois collegues dans la BD

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -5,22 +5,31 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import dev.top.entities.Collegue;
 import dev.top.entities.Version;
+import dev.top.repos.CollegueRepo;
 import dev.top.repos.VersionRepo;
 
 @Component
 public class StartupDataInit {
 
-    @Autowired
-    VersionRepo versionRepo;
+	@Autowired
+	VersionRepo versionRepo;
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void init() {
+	@Autowired
+	CollegueRepo collegueRepo;
 
-        this.versionRepo.save(new Version("v1"));
-        this.versionRepo.save(new Version("v2"));
-        this.versionRepo.save(new Version("v3"));
-        this.versionRepo.save(new Version("v4"));
+	@EventListener(ContextRefreshedEvent.class)
+	public void init() {
 
-    }
+		this.versionRepo.save(new Version("v1"));
+		this.versionRepo.save(new Version("v2"));
+		this.versionRepo.save(new Version("v3"));
+		this.versionRepo.save(new Version("v4"));
+
+		this.collegueRepo.save(new Collegue(0, "Toto", "www.photo.fr"));
+		this.collegueRepo.save(new Collegue(0, "Titi", "www.photo.com"));
+		this.collegueRepo.save(new Collegue(0, "Tata", "www.photo.net"));
+
+	}
 }
